793dd934595d01194864f33c88a31c97896e3462
Open Provenance <steve@myveryown.net>
2016-02-28 14:45:36 +0100

    Create 02-CTransaction.py

---
+++ 02-CTransaction.py
@@ -0,0 +1,41 @@
+### Open Provenance February 2016 - https://myveryown.org
+### Bitcoin Blockchain Information using python-bitcoinlib
+### CTransaction Object and Properties
+
+## Import the modules required and setup a connection to bitcoin
+import bitcoin
+
+## Create a proxy object and connect to the bitcoin.rpc 
+import bitcoin.rpc
+myproxy = bitcoin.rpc.Proxy()
+
+## Get the latest CBlock data from bitcoin rpc proxy
+block_info = myproxy.getblock(myproxy.getblockhash(myproxy.getblockcount()))
+
+## From the CBlock object we are able to get the transactions
+vtx = block_info.vtx
+
+## Print the details to the screen.
+print "----------------------------------------------------------------"
+print "Bitcoin CTransaction Object Information: Block Height ", myproxy.getblockcount()
+print "----------------------------------------------------------------"
+
+## We assume there could be many transactions in a block and loop through them
+## we would loop all transaction with 'for x in range (0, len(vtx)) :'
+## but in this example we will show the first or "coinbase" transaction details.
+## 
+if len(vtx) > 0 :
+  for x in range (0, 1) :
+
+	## Isolate the CTransaction Object
+	thetx = vtx[x]
+
+	## Now we have the object we can get information from it
+	print "Is Coinbase: ", thetx.is_coinbase()
+	print "nVersion: ", thetx.nVersion
+	print "nLockTime: ", thetx.nLockTime
+	print "TxHash: "
+	print bitcoin.core.b2lx(thetx.GetHash())
+print "----------------------------------------------------------------"
+print " "  
+exit()
